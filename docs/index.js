bacf00a777baf2c95dcece9eb0058e6807862822
moandji.ezana <mwanji@gmail.com>
2015-11-26 09:09:50 -0400

    Make Toggle.Item label a prop. Add Toggle.Item.propTypes

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,5 @@
 const off = <i>First</i>
-var checkedItem = "month";
+var checkedItem = "year";
 
 function onChange(id) {
   console.log(id);
@@ -10,10 +10,10 @@ function render() {
   ReactDOM.render(<div>
     <div><ToggleSwitch.Light title="abc" off={off} on="second" theme="ios" /></div>
     <div>
-      <ToggleSwitch.Toggle name="view" theme="candy" blue onChange={onChange} checked={checkedItem}>
-        <ToggleSwitch.Toggle.Item id="week" value="w">Week</ToggleSwitch.Toggle.Item>
-        <ToggleSwitch.Toggle.Item id="year" value="y" checked>Year</ToggleSwitch.Toggle.Item>
-        <ToggleSwitch.Toggle.Item id="month" value="m">Month</ToggleSwitch.Toggle.Item>
+      <ToggleSwitch.Toggle name="view" theme="candy" blue onChange={onChange} checkedItem={checkedItem}>
+        <ToggleSwitch.Toggle.Item id="week" value="w" label="Week" />
+        <ToggleSwitch.Toggle.Item id="year" value="y" label="Year" checked />
+        <ToggleSwitch.Toggle.Item id="month" value="m" label="Month" />
       </ToggleSwitch.Toggle>
     </div>
   </div>, document.getElementById("examples"));
--- src/Toggle.jsx
+++ src/Toggle.jsx
@@ -21,11 +21,12 @@ class Toggle extends React.Component {
       "switch-candy-blue": this.props.blue === true,
       "switch-candy-yellow": this.props.yellow === true
     });
+    
   
     const children = React.Children.map(this.props.children, (child) => {
       return [
         <input key={child.props.id + "-input"} id={child.props.id} name={this.props.name} type="radio" checked={this.state.checkedItem === child.props.id} onChange={this.onChange.bind(this, child.props.id)} />,
-        <label key={child.props.id + "-label"} htmlFor={child.props.id} onclick="">{child.props.children}</label>
+        <label key={child.props.id + "-label"} htmlFor={child.props.id} onclick="">{child.props.label}</label>
       ];
     }, this);
   
@@ -51,10 +52,17 @@ class ToggleItem extends React.Component {
   render() {
     return (<span>
       <input id={this.props.id} name={this.props.name} type="radio" defaultChecked={this.props.checked} />
-      <label htmlFor={this.props.name} onclick="">{this.props.children}</label>
+      <label htmlFor={this.props.name} onclick="">{this.props.label}</label>
       </span>);
   }
 }
+
+ToggleItem.propTypes = {
+  id: React.PropTypes.string.isRequired,
+  label: React.PropTypes.any.isRequired,
+  value: React.PropTypes.any,
+  checked: React.PropTypes.bool
+};
 
 Toggle.Item = ToggleItem;
  
